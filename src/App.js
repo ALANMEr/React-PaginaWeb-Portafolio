ac56f5946182108a2b45497a2b35b74f44442234
ALANMer <alanmercadoamg@gmail.com>
2022-12-02 20:09:41 -0600

    Finish Page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react'
-import { About } from './components/about/About'
+import  About  from './components/about/About'
 import ScrollToTop from "react-scroll-to-top";
-import { Home } from './components/Home/Home'
-import { Navbar } from './components/Navbar/Navbar'
+import  {Home } from './components/Home/Home'
+import  Navbar  from './components/Navbar/Navbar'
 import { Service } from './components/service/Service'
 import Testimonial from './components/testimonial/Testimonial'
-import Subscription from './subscriptions/Subscription'
+import Subscription from './components/subscriptions/Subscription'
 import Footer from './components/footer/Footer';
 
 import Team from './components/Team/Team';
 import Feedback from './components/feedback/Feedback';
+import Contact from './components/Contact/Contact';
 
 
 const App = () => {
@@ -23,6 +24,7 @@ const App = () => {
       <Feedback />
       <Subscription />
       <Team />
+      <Contact/>
       <Footer />
       <ScrollToTop smooth
         height='20'
