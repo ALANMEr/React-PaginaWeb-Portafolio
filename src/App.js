d4dbd5831640974d31aa2b43846812ba87bdd46c
ALANMer <alanmercadoamg@gmail.com>
2022-12-02 19:04:21 -0600

    Slider

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React from 'react'
 import { About } from './components/about/About'
-
+import ScrollToTop from "react-scroll-to-top";
 import { Home } from './components/Home/Home'
 import { Navbar } from './components/Navbar/Navbar'
 import { Service } from './components/service/Service'
 import Testimonial from './components/testimonial/Testimonial'
 import Subscription from './subscriptions/Subscription'
+import Footer from './components/footer/Footer';
+
+import Team from './components/Team/Team';
+import Feedback from './components/feedback/Feedback';
+
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <Home/>
-    <About/>
-    <Service/>
-    <Testimonial/>
-    <Subscription/>
+      <Home />
+      <About />
+      <Service />
+      <Testimonial />
+      <Feedback />
+      <Subscription />
+      <Team />
+      <Footer />
+      <ScrollToTop smooth
+        height='20'
+        width='20'
+        style={{ borderRadius: "90px" }} />
     </>
 
   )
